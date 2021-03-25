6ca64b1f86699fe4d22df691c96a8c00b9c1f722
kent <gkh1974@gmail.com>
2021-03-25 15:57:42 -0400

    added logo text

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 import Svg from "../../../components/Svg/Svg";
 import { SvgProps } from "../../../components/Svg/types";
+import styled from "styled-components";
 
 interface LogoProps extends SvgProps {
   isDark: boolean;
 }
 
+const StyledText = styled.div`
+   width: 230px;
+   display: flex;
+  .logo-main-text {
+    font-size: 50px;
+    margin-left: 12px;
+    color: #cdb537;
+  }
+`;
+
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 36 33" {...props}>
-      <image height="33" href={'/images/dino/logo-with-text.png'}/>
-    </Svg>
-
+    <StyledText>
+      <Svg viewBox="0 0 36 33" {...props}>
+        <image height="33" href={'/images/dino/logo-with-text.png'}/>
+      </Svg>
+      <span className="logo-main-text" style={{fontFamily:'JurassicPark'}}>JurassicFarm</span>
+    </StyledText>
   );
 };
 
